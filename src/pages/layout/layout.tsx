4f48ce7fb8101f37123b6074f7fbe817107faa49
Hwlhwlxyz <huweilun1994@hotmail.com>
2023-04-08 20:10:00 -0400

    change sidebar behavior

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,16 +1,36 @@
 import Link from 'next/link'
-import { push as Menu } from 'react-burger-menu'
+import { slide as Menu } from 'react-burger-menu'
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from 'react';
 
 export default function Layout({ children }:any) {
   const [isServer, setIsServer] = useState(true)
+const [marginLeftNumber, setmarginLeftNumber] = useState('10px');
+const [menuOpen, setMenuOpen] = useState(false);
+
+
   useEffect(() => {
     setIsServer(typeof window === 'undefined')
     console.log('ClientSide: isServer', isServer)
   }, [])
+  function handleOnOpen(): void {
+    setmarginLeftNumber('300px')
+    setMenuOpen(true)
+  }
+
+  function handleOnClose(): void {
+    setmarginLeftNumber('0')
+    setMenuOpen(false)
+  }
+
   return (<div id="outer-container">
-      {!isServer&&<Menu noOverlay noTransition className='bm-menu-wrap' pageWrapId={'page-wrap'} outerContainerId={ "outer-container" } >
+      {!isServer&&<Menu 
+      noOverlay noTransition className='bm-menu-wrap' 
+      pageWrapId={'page-wrap'} outerContainerId={ "outer-container" } 
+      onOpen={ handleOnOpen }
+      onClose={ handleOnClose }
+      isOpen={ menuOpen }
+      >
         <Link id="home" className="menu-item" href="/">Home</Link>
         <Link id="json" className="menu-item" href="/tools/jsonviewer">Json Viewer</Link>
         <Link id="encode" className="menu-item" href="/tools/encode">Encode Decode</Link>
@@ -18,7 +38,10 @@ export default function Layout({ children }:any) {
         <a id="contact" className="menu-item" href="/contact">Contact</a>
         {/* <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
       </Menu>}
-      <main id="page-wrap" className={styles.main}>{children}</main>
+      <main id="page-wrap" className={styles.main}
+      style={{marginLeft: marginLeftNumber}}
+      
+      >{children}</main>
     </div>
   )
 }
